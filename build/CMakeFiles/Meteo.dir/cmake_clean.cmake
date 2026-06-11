d8808d6f5efe872f0b7048954b6cbe4c6c3cd87b
Lionjj <danilo.ivone.18@gmail.com>
2026-06-11 20:19:46 +0200

    Ambiente configurato per l'inzio del progetto.

---
+++ build/CMakeFiles/Meteo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Meteo.dir/src/main.cpp.o"
+  "CMakeFiles/Meteo.dir/src/main.cpp.o.d"
+  "Meteo"
+  "Meteo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Meteo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "Ambiente configurato!" << std::endl;
+    return 0;
+}
